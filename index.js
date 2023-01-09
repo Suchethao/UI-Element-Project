cf35e18730606b418ea55f0a85e0338477d83477
Suchethao <116967144+Suchethao@users.noreply.github.com>
2023-01-09 17:28:12 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// const tileBoard = document.querySelector (".js-cards")
+// const tile = document.querySelectorAll (".js-card")
+// const reset = document.querySelector (".js-reset");
+// {/* // When the user clicks on <div>, open the popup */}
+
+function expand(e) {
+  let popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
